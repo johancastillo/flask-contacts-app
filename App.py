d7a0e9458aed7fc108f6b8a8a271f4f32dab1415
Johan Castillo <jcjohan2707@gmail.com>
2020-12-19 21:26:00 -0400

    My first route

--- App.py
+++ App.py
@@ -4,6 +4,11 @@ from flask import Flask
 # Instance of Flask class
 app = Flask(__name__)
 
+# Routes of the application
+@app.route('/')
+def home():
+    return "Hello World"
+
 # Run server
 if __name__ == '__main__':
     app.run(port = 4000, debug = True)
