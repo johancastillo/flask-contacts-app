974e47c002bbe9e2ab67bbc98d56c469fd6faabc
Johan Castillo <jcjohan2707@gmail.com>
2020-12-20 19:23:16 -0400

    Sentences modificated

--- database.sql
+++ database.sql
@@ -1,7 +1,22 @@
+/* Dalete Data Base if exists */
+DROP DATABASE IF EXISTS flaskcontacts;
+
+/* Create Data Base */
+CREATE DATABASE flaskcontacts;
+USE flaskcontacts;
+
 /* Definition of the table contacts */
-create table contacts (
+CREATE TABLE contacts (
     id INT PRIMARY KEY AUTO_INCREMENT,
     fullname VARCHAR(255),
     phone VARCHAR(255),
     email VARCHAR(255)
 );
+
+/* Inserting data in the table */
+INSERT INTO contacts (fullname, phone, email) 
+VALUES ("test", "test", "test");
+
+/* Validations */
+SHOW TABLES;
+SELECT * FROM contacts;
